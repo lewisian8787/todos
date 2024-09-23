d76c1ed1a6ff8a3c513248ed51fcea90b917c4eb
Ian Lewis <lewisian8787@gmail.com>
2024-09-23 16:16:21 +0000

    final todos commit

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,9 +6,15 @@ $(function() {
         
         var ok = confirm("Are you sure homie? Dis be forever")
         if (ok) {
-            this.submit();
+            //this.submit();
+            
+            var form = $(this);
+            
+            $.ajax({
+                url: form.attr("action"),
+                method: form.attr("method")
+            })   
         }
-    
     });
     
 });
